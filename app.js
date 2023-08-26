9d85406101f71066e29ca4a27da4b554a607547c
Jason Gerard <jasongerard321@gmail.com>
2023-08-26 18:02:51 -0400

    Add base files

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+    res.type('html').send("<h1>Hello World</h1>");
+});
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
